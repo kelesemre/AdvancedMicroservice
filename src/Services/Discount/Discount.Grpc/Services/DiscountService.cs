dfea4ea64a2d6a2747bbf7e9d0c81cff7b8126d5
kelesemre <emrekeless@gmail.com>
2022-03-27 18:37:25 +0300

    DiscountProto part 1

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repository;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
